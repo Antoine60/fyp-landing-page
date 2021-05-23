5653f598ac3ba8bbe12459a5820429db73c174f6
Antoine60 <antoine.gillot60@gmail.com>
2021-05-23 15:52:02 +0200

    Anchors, contact form style

--- js/main.js
+++ js/main.js
@@ -23,3 +23,24 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
+
+$(document).ready(function() {
+
+//Smooth scroll
+    const links = document.querySelectorAll(".navbar-menu a.navbar-item");
+
+    for (const link of links) {
+        link.addEventListener("click", clickHandler);
+    }
+
+    function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+
+    }
+});
